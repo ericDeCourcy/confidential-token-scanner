0892a581695923955bd416f053c932733081f846
Eric DeCourcy <eric@openzeppelin.com>
2026-02-27 19:32:31 -0700

    Try to add db and fix up readme

--- README.md
+++ README.md
@@ -78,6 +78,58 @@ There are a few things you can reconfigure
 
 A simple way to reconfigure this is a #TODO item
 
+### Discoveries
+
+#### 0.2% (95 of 47000) of transfers are to non-zama contracts
+- This means the majority are just interacting with the auction contract, which is NOW traceable. 
+  - The auction contract was NOT traceable at the time of the auction - the privacy function of the auction was to hide the settlement price. But the settlement price was used for all bidders.
+- Out of rougly 47k transfers recorded, only about 100 of them were to addresses that weren't the wrapper or auction contracts.
+- Many accounts involved in these transfers show very little activity. "Interesting" transactions are between two non "safe" contracts (safe contracts being wrappers, auction contracts, zama distributor contracts, etc.)
+- Apparently heavily correlated with people actually calling the dang transfer function 
+```
+Fully external transfers
+95
+
+---------------------- Address ----------------------------------------|-------- number of interesting txs 
+https://etherscan.io/address/0x5e310f01a0b13278cf676b3439d32859e0aad82d 17
+https://etherscan.io/address/0x5c178c08363928c6296ed420eaeef73e21b667d4 11
+https://etherscan.io/address/0x1dc385c0594358886a1b21eda0ad4c053214c699 6
+https://etherscan.io/address/0x77b7d6545d352bfd858b308343d1a4e414cb4d7e 6
+https://etherscan.io/address/0xc695d7097a6a4208b33cc7b85f8a6844a90977dd 6
+https://etherscan.io/address/0xb905bce0188045fde5aab20742ec17e9ab6dd853 6
+https://etherscan.io/address/0xf38d9f73c6bcb9e047082442580f332e3cbcedce 6
+https://etherscan.io/address/0x3d9a867c1ff6bbac2a01dc2678cd7819216874f6 4
+https://etherscan.io/address/0x6d7b5a32fc63c5011c3dd217e151c9c118908dfd 4
+https://etherscan.io/address/0x36afce8f48bbb961c76ec20bc07f34f313374fd9 4
+https://etherscan.io/address/0x3a947148972087dd89c2fda6f0a8cb915948457b 4
+https://etherscan.io/address/0x441319b8e436e3fe7d2b685377980fdba203cbec 4
+https://etherscan.io/address/0x4978a0d5e2e582a03bc58cdf7a8e89639756c74b 4
+https://etherscan.io/address/0x2c2a9ab5922632b397495d56b66a276e09c42d91 4
+https://etherscan.io/address/0x3a531341103d589aea7481be75edbca2e9a69605 3
+https://etherscan.io/address/0xf13a99222b7e613855cdbc9a667973af7aa202fe 2
+https://etherscan.io/address/0x14db914aec49981153c7b16bf85d2ac997c34133 2
+https://etherscan.io/address/0xeb7e54b34548a2a8d924fc323f897cd42425b525 2
+https://etherscan.io/address/0x17e53556fdda3bf5e53b73af1b68cfcedadd6b1c 2
+https://etherscan.io/address/0xfdad746daecfb2e58bc5c6b3ca7aa208081a600f 2
+https://etherscan.io/address/0xc91a004d5baa708230527283bfaee3e1d19100b2 2
+https://etherscan.io/address/0x3776d95fbb1859de65b54204a9778c8bb19d2873 2
+https://etherscan.io/address/0xc54489677956fdc2acb376185761a858ce85544a 2
+https://etherscan.io/address/0x3c8d0ef4f5fe05cfcd60d98e35ee493a0c12f21c 2
+https://etherscan.io/address/0x255cdddbec2c76b2f409248abeea49c1b1c6bf18 2
+```
+
+You wanna run this for yourself??? Huh?? Do ya?
+
+You'll need a events.db for this
+
+run:
+```
+node scripts/total-external-transfers.js
+```
+
+
+
+
 ### Future/Planned work
 
 - [ ] Easy configuration of params network, token address, and block range. Also adding starting and final block.
--- scripts/total-external-transfers.js
+++ scripts/total-external-transfers.js
@@ -10,11 +10,53 @@ let unknownTotal = 0;
 
 async function main() {
 
+    /*
     const input = process.argv[2];
     if (!input) {
       console.error("Usage: node total-external-transfers.js <0xAddress>");
       process.exit(1);
     }
+      */
+
+    const listOfAuctionWallets = [
+        "0000000000000000000000000000000000000000",
+        "2c7f968107a47087085308e7833dd8b11ecddf4f",
+        "ef7857c5a1fc254c6883771eb15e119596a6d65a",
+        "e3cfd946b719d6c759944bcca4f9d370d50a7f49",
+        "f8a376aa3214841243831ad18f2367fae831acf9",
+        "70980569ebd236f557211a8bc20c0b5845034502",
+        "508459465da5cf760ccbf2a0acd156f40383770e",
+        "e01c8a7f1877f1609c447fee2ee963d1853266bb",
+        "6af14a92a6a1cf2b694fd37aaca6ec5b36eabecc",
+        "2dbfc8f37d7cd52d5a28fac4799bb932446f3f8a",
+        "93345f1976eb1575ac03ab4f23728ac9029d250b",
+        "54fd9ab08078cf60e168838ed2597c2682d26b84",
+        "6df8246cf8f084101a60c89966e2dc7cc672021f",
+        "1fe756179e61cb6d89c3d3af82376f0031be0edf",
+        "a5c8c4666c4a2567ed01e41b5d98839bdda458b7",
+        "47cf5283fd2fc5cf46614c9f593273cebac7fc4e",
+        "03b9d091bb9854cd9813ad6e29cc5d5ec64943db",
+        "fcdc336df1201df580b62fcdc0c456ad41e2706b",
+        "ae7bd990a559b35bd12ad3c3f0bd86c068c50115",
+        "1b411cc4d55a4ab2302f0e4f6aec5e3d66b29111",
+        "056f0498268a497d66ab2843c8bb8edebb01608e",
+        "6fcc0995b6a691b7b4652458df8067a985549bcf",
+        "9f6a86fc62597bcccf56d9400078ba6b8a7093d3",
+        "1dfd06a53f4392a1e8ddaa315637944a1d6953d1",
+        "a4e7442d3ce8d688d0854b23e9af9f19e9a49e07",
+        "ea5c7a58f446235e2d3b34587ce525e784a199a7",
+        "2615b27d6e7a450e03bf5c94f6141c507e934f41",
+        "91d8e75d14f7c0f1fdfb362ce70d3b5632b7e1ea",
+        "9700d6e978107fb91ff634b7b397cf7d8ecc7681",
+        "1a1dc272a0f894e4a0574ce9c88a9eadcf46aa40",
+        "bacbe998075640ba12c1f763c8e67fbda6016f48",
+        "1576ffc274046e63028316ce15807227ba9da5ce",
+        "b5d8bd9b45c9eac6f51cb4da75d49901d4468255",
+        "9cf3c00bd77d710a148133815ff0684d3b624598", //APPArently this is a zama deployer...??? It has a few "exotic" transactions
+    ];
+
+
+
     
     // open db
     const db = new Database("events.db", { readonly: true });
@@ -24,76 +66,74 @@ async function main() {
     const stmt = db.prepare(`
         SELECT topic1, topic2   
         FROM contract_logs
-        WHERE topic1 LIKE ?
-           OR topic2 LIKE ?
-      `);
-      
-    const term = "0x000000000000000000000000"+input.slice(-40);
-    console.log(`term ${term}`);
     
-    const rows = await stmt.iterate(term, term);
+    `);
+      
+    //const term = "0x000000000000000000000000"+input.slice(-40);
+    //console.log(`term ${term}`);
+    
+    const rows = await stmt.iterate();
 
-    const countsOfExternal = new Map();
+    const countsOfAddresses = new Map();
 
     function add(item) {
-      countsOfExternal.set(item, (countsOfExternal.get(item) || 0) + 1);
+        countsOfAddresses.set(item, (countsOfAddresses.get(item) || 0) + 1);
     }
 
-    console.log("starting");
+    // This means that the list is 
+    const nonSafelistTransfers = new Map();
 
-    for(const row of rows) {
-        if(row.topic1 == "0x000000000000000000000000"+input.slice(-40))
-        {
-            console.log("abc");
-            if
-            ( 
-                row.topic2 == "0x0000000000000000000000000000000000000000000000000000000000000000" //minter
-                || row.topic2 == "0x00000000000000000000000004a5b8C32f9c38092B008A4939f1F91D550C4345" //auction address
-            )
-            {
-                safeOnlyTotal++;
-                console.log(`safeOnlyTotal ${safeOnlyTotal}`);
-            }
-            else
-            {   
-                add(row.topic2);
-                externalTransferTotal++;
-                console.log(`externalTransferTotal ${externalTransferTotal}`);
-            }
-        }
-        else if(row.topic2 == "0x000000000000000000000000"+input.slice(-40))
-        {
-            console.log("def");
-
-            if
-            ( 
-                row.topic1 == "0x0000000000000000000000000000000000000000000000000000000000000000" //minter
-                || row.topic1 == "0x00000000000000000000000004a5b8C32f9c38092B008A4939f1F91D550C4345" //auction address
-            )
-            {
-                safeOnlyTotal++;
-                console.log(`safeOnlyTotal ${safeOnlyTotal}`);
-            }
-            else
-            {   
-                add(row.topic2);
-
-                externalTransferTotal++;
-                console.log(`externalTransferTotal ${externalTransferTotal}`);
-
-            }
-        }
-        else
-        {
-            console.log("xyz");
-
-            unknownTotal++;
-            console.log(`unknownTotal ${unknownTotal}`)
-        }
-
+    function addNonSafeList(item) {
+        nonSafelistTransfers.set(item, (nonSafelistTransfers.get(item) || 0) + 1);
     }
 
+    for(const row of rows)
+    {
+        add(row.topic1);
+        add(row.topic2);
 
+        const flagged = listOfAuctionWallets.some(value => row.topic1.includes(value));
+        const flaggedAlso = listOfAuctionWallets.some(value => row.topic2.includes(value));
+        if(!flagged && !flaggedAlso) { 
+            externalTransferTotal++; 
+            console.log(`topic1, topic2 = {${row.topic1.slice(-40)},${row.topic2.slice(-40)}}`)
+            
+            addNonSafeList(row.topic1);
+            addNonSafeList(row.topic2);
+        }
+
+        
+    }
+
+    // just enough to see the dropoff in auction wallets lol
+    const top40 = [...countsOfAddresses.entries()]
+        .sort((a, b) => b[1] - a[1])
+        .slice(1, 41);          // slices off "zero address" because it has by far the most due to mints/burns
+
+
+    for (const [address, count] of top40) {
+        const formattedLink = `https://etherscan.io/address/${"0x"+address.slice(-40)}`;
+        console.log(formattedLink, count);
+    }
+
+    console.log("=-=-=-=-=-=-=")
+    console.log("\nFully external transfers");
+
+    console.log(externalTransferTotal)
+
+
+    const top25 = [...nonSafelistTransfers.entries()]
+        .sort((a, b) => b[1] - a[1])
+        .slice(0, 25);         
+
+
+    for (const [address, count] of top25) {
+        const formattedLink = `https://etherscan.io/address/${"0x"+address.slice(-40)}`;
+        console.log(formattedLink, count);
+    }
+
+    console.log(externalTransferTotal)
+   
     } 
 
 
@@ -103,7 +143,7 @@ async function main() {
         db.close();
     } 
 
-    console.log(`${safeOnlyTotal} ${externalTransferTotal}`);
+    //console.log(`${safeOnlyTotal} ${externalTransferTotal}`);
 
 }
 
